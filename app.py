236b94b64cec3cd5b95fa8df55d577ed6fc632f6
Hihanghohengg <yohanesobed1@gmail.com>
2026-08-11 10:42:02 +0700

    Trick ZeroGPU into staying alive

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ import os
 import uvicorn
 from server.main import app
 
+try:
+    import spaces
+    @spaces.GPU
+    def dummy_gpu_function():
+        pass
+except ImportError:
+    pass
+
 # Hugging Face Spaces (Gradio SDK) selalu mencari file app.py dan membuka port 7860.
 # Dengan file ini, kita "mengelabui" sistem Gradio agar menjalankan FastAPI kita di port tersebut secara gratis!
 
